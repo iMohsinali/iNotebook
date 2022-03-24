fe273ced441cb1c8f70c4d89ca9e68954218ef23
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-03-24 12:19:49 +0500

    login
    user bycrtpt to hasnh password and user jwt for token

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -2,8 +2,11 @@ const express = require("express");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "Mohsin2@12145%";
 router.post(
-  "/",
+  "/createuser",
   [
     body("email").isEmail(),
     // password must be at least 5 chars long
@@ -16,19 +19,67 @@ router.post(
       if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
       }
-
-      await User.create({
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "email aldrey exist" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const secure = await bcrypt.hash(req.body.password, salt);
+      user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secure,
       });
-      await res.send(req.body);
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = jwt.sign(data, JWT_SECRET);
+
+      await res.send(token);
     } catch (err) {
       console.log("err");
-
-      res.json({ error: "pleae enteer unqiue email" });
     }
   }
 );
 
+//login router
+router.post(
+  "/login",
+  [
+    body("email", "enter valid email").isEmail(),
+    // password must be at least 5 chars long
+
+    body("password", "password cannt be blank").exists(),
+  ],
+  async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Plesase enter correct credetional" });
+      }
+      const passwordcampare = await bcrypt.compare(password, user.password);
+      if (!passwordcampare) {
+        return res
+          .status(400)
+          .json({ error: "Plesase enter correct credetional" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = jwt.sign(data, JWT_SECRET);
+
+      await res.send(token);
+    } catch (error) {
+      console.log("internal server error");
+    }
+  }
+);
 module.exports = router;
