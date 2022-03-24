d850be4d4007ba5e8061fce6d0a04fcd3d24d1b4
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-03-24 19:13:00 +0500

    fetch and update notes

--- Backend/Routes/notes.js
+++ Backend/Routes/notes.js
@@ -1,11 +1,65 @@
 const express = require("express");
+const fetchuser = require("../middleware/fetchuser");
+const { body, validationResult } = require("express-validator");
+const Note = require("../models/Note");
 const router = express.Router();
-router.get("/", (req, res) => {
-  obj = {
-    name: "this is note",
-    rool: 34,
-  };
-  res.json(obj);
+
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  const notes = await Note.find({ user: req.user.id });
+  res.json(notes);
 });
 
+router.post(
+  "/addnotes",
+  fetchuser,
+  [
+    body("title").isLength({ min: 3 }),
+    body("descreption").isLength({ min: 10 }),
+  ],
+  async (req, res) => {
+    const { title, descreption, tag } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const notes = new Note({ title, descreption, tag, user: req.user.id });
+    const saveote = await notes.save();
+    res.json(saveote);
+  }
+);
+
+router.put(
+  "/updatenotes/:id",
+  fetchuser,
+
+  async (req, res) => {
+    const { title, descreption, tag } = req.body;
+    //new note
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (descreption) {
+      newNote.descreption = descreption;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+    //note to be update
+
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+    note = await Note.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+    res.json(note);
+  }
+);
 module.exports = router;
