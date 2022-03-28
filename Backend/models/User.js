2c590ae36233a56563c5056c7aa2660ac82d7f26
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-03-28 14:07:01 +0500

    SyntaxError: JSON.parse: unexpected character at line 1 column 1 of the JSON data

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -4,7 +4,6 @@ const { Schema } = mongoose;
 const UserSchema = new Schema({
   name: {
     type: String,
-    required: true,
   },
   email: {
     type: String,
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import { Alert } from "./components/Alert";
 import Home from "./components/Home";
+import Login from "./components/Login";
 import Navbar from "./components/Navbar";
+import Signup from "./components/Signup";
 
 import NotesState from "./Context/notes/NotesState";
 const App = () => {
@@ -15,6 +17,8 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route exact path="/about" element={<About />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/login" element={<Login />} />
           </Routes>
         </BrowserRouter>
       </NotesState>
